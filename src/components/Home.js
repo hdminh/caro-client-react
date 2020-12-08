1db755d0e209349507bf8c910b4f0fbca43b2f4a
hdminh <minhcine99@gmail.com>
2020-12-08 22:45:12 +0700

    add socketio to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
 import React,{ useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { ACCESS_TOKEN_NAME } from '../constants/apiContants';
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
 function Home(props) {
+    const ioClient = io.connect("http://127.0.0.1:8080");
+    const token = localStorage.getItem("token")
+    const [ data, setData] = useState("")
+    ioClient.emit("online",{ token: token })
     useEffect(() => {
-        if (!localStorage.getItem(ACCESS_TOKEN_NAME)) redirectToLogin()
-    })
+        if (!localStorage.getItem(ACCESS_TOKEN_NAME)) redirectToLogin();
+        ioClient.on("statusFriendChange",(data)=>{
+            setData(JSON.stringify(data))
+          })
+    }, [])
     function redirectToLogin() {
     props.history.push('/login');
     }
-    return(
-        <div className="mt-2">
-            Home page content
-        </div>
-    )
+  return (
+    <div className="App">
+      {data}
+    </div>
+  );
 }
 
 export default withRouter(Home);
