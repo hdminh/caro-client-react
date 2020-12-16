852e53afc47011bbc3f9b1a4b74aefe6cb0c13c3
minh.ho <minh.ho@mservice.com.vn>
2020-12-16 18:09:37 +0700

    refactor

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,4 @@
 import React,{ useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 import { ACCESS_TOKEN_NAME } from '../constants/apiContants';
 import { getRoomInfo } from '../utils/api';
@@ -9,7 +7,7 @@ import io from 'socket.io-client'
 const ioClient = io.connect("https://caro-game-api.herokuapp.com/");
 //const ioClient = io.connect("http://127.0.0.1:8080");
 
-function Home(props) {
+function Room(props) {
     const token = localStorage.getItem(ACCESS_TOKEN_NAME)
     if(token){
       ioClient.emit("online",{ token: token })
@@ -32,26 +30,6 @@ function Home(props) {
   function redirectToLogin() {
     props.history.push('/login');
   }
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
-  const classes = useStyles();
   return (
     <div className="App">
         {data}
@@ -59,4 +37,4 @@ function Home(props) {
   );
 }
 
-export default withRouter(Home);
+export default withRouter(Room);
